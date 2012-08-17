c7ab05cbd9218a7d93182ece11e7467e612b12ef
makaveli80 <tom.briois@gmail.com>
2012-08-17 17:30:56 +0200

    ajout de champs de recherches + scopes pour la recherche dans les produits

--- app/controllers/api/catalog/products_controller.rb
+++ app/controllers/api/catalog/products_controller.rb
@@ -11,16 +11,18 @@ class Api::Catalog::ProductsController < Api::ApiController
   # ====Result:
   # * "+collection of Products+"
   def index
-    products = Product.includes(:category, :picture).all
 
-    if products
+    products, errors = get_products_with_request
+
+    if products && errors.empty?
       response = formatted_response(true, {:products => products}, t('api.message.catalog.products.index.success'))
     else
-      response = formatted_response(false, nil, t('api.message.catalog.products.index.failure'))
+      response = formatted_response(false, {:errors => errors}, t('api.message.catalog.products.index.failure'))
     end
 
     render :json => response
   end
+
 
   # Get a Product
   #
@@ -123,6 +125,59 @@ class Api::Catalog::ProductsController < Api::ApiController
     end
 
     render :json => response
+  end
+
+  protected
+
+  def get_products_with_request
+    errors = []
+
+    # test si un "scope" est passé en paramètres
+    if defined? params[:scope] then
+      begin
+        scope = params[:scope]
+
+        # recherche par rapport à un texte
+        if scope =~ /search_(.*)/
+          text = params[:text]
+          if text
+            products = Product.send(scope, text).with_category_and_picture
+          else
+            errors << t('api.message.catalog.products.index.errors.no_text')
+          end
+
+        # recherche par rapport au prix
+        elsif scope == "price"
+          min_price = params[:min_price]
+          max_price = params[:max_price]
+
+          if min_price and max_price
+            products = Product.price_between(min_price, max_price).with_category_and_picture
+          elsif min_price
+            products = Product.price_min(min_price).with_category_and_picture
+          elsif max_price
+            products = Product.price_max(max_price).with_category_and_picture
+          else
+            errors << t('api.message.catalog.products.index.errors.no_price')
+          end
+
+        end
+
+      # "scope" non trouvé
+      rescue NoMethodError => e
+        errors << e.to_s.split(" for")[0]
+
+      # erreur générique
+      rescue Exception => e
+        errors << e.to_s
+      end
+
+    # sans "scope" => tous les produits
+    else
+      products = Product.with_category_and_picture
+    end
+
+    return products, errors
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -15,6 +15,29 @@ class Product < ActiveRecord::Base
   validates_presence_of     :name, :price, :stock, :reference
   validates_numericality_of :price, :stock, :greater_than_or_equal_to => 0
 
+
+  #===========================================================================#
+  #===   SCOPES
+  #===========================================================================#
+  scope :with_category_and_picture, includes(:category, :picture)
+
+  # Stocks
+  scope :with_stock,                where("products.stock > 0")
+  scope :without_stock,             where("products.stock <= 0")
+
+  # Search text
+  scope :search_text,               lambda { |text| where("reference like ? or label like ? or name like ? or description like ?",
+                                                          "%#{text}%", "%#{text}%", "%#{text}%", "%#{text}%")  }
+  scope :search_reference,          lambda { |text| where("reference like ?", "%#{text}%")  }
+  scope :search_label,              lambda { |text| where("label like ?", "%#{text}%")  }
+  scope :search_name,               lambda { |text| where("name like ?", "%#{text}%")  }
+  scope :search_description,        lambda { |text| where("description like ?", "%#{text}%")  }
+
+  # Search prices
+  scope :price_max,                 lambda { |max_price| where("price <= ?", max_price)  }
+  scope :price_min,                 lambda { |min_price| where("price >= ?", min_price)  }
+  scope :price_between,             lambda { |min_price, max_price| where("price >= ? and price <= ?", min_price, max_price)}
+
   #===========================================================================#
   #===   CALLBACKS
   #===========================================================================#
