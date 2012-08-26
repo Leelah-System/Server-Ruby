68ebb461ae1b994beac08c1b57c0ea7bfd823fea
Vanessa <vanessa.pasque@gmail.com>
2012-08-26 16:13:23 +0200

    Import seed for users, categories and products - Restaurant database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,52 @@
+require 'open-uri'
+
 # Users creation
 User.delete_all
-
-users = User.create([
-  {:login => "thomas",  :password => "thomas",  :first_name => "Thomas",  :last_name => "Briois", :email => "tom.briois@gmail.com", :status => User::STATUS_ADMIN},
-  {:login => "john",    :password => "john",    :first_name => "John",    :last_name => "Doe",    :email => "john.doe@gmail.com",   :status => User::STATUS_NONE}
-])
-
+nb_users = 0
+open("http://doc.leelah-system.com/base/restaurant1/users.csv") do |users|
+  users.read.each_line do |user|
+    if nb_users == 0
+      # Do not insert the first line
+      nb_users += 1
+    else
+      login, password, first_name, last_name, email, status = user.chomp.split(";")
+      user.create!(:login => login, :password => password, :first_name => first_name, :last_name => last_name, :email => email, :status => status)
+      nb_users += 1
+    end
+  end
+end
 puts "Users created: #{users.size} user(s)"
+
+# Categories creation
+Category.delete_all
+nb_categories = 0
+open("http://doc.leelah-system.com/base/restaurant1/categories.csv") do |categories|
+  categories.read.each_line do |category|
+    if nb_categories == 0
+      # Do not insert the first line
+      nb_categories += 1
+    else
+      id, label, name, description = category.chomp.split(";")
+      category.create!(:id => id, :label => label, :name => name, :description => description)
+      nb_categories += 1
+    end
+  end
+end
+puts "Categories created: #{categories.size} categories"
+
+# Products creation
+Product.delete_all
+nb_products = 0
+open("http://doc.leelah-system.com/base/restaurant1/products.csv") do |products|
+  products.read.each_line do |product|
+    if nb_products == 0
+      # Do not insert the first line
+      nb_products += 1
+    else
+      id, reference, label, name, description, price, stock, logo_id, category_id = product.chomp.split(";")
+      product.create!(:id => id, :reference => reference, :label => label, :name => name, :description => description, :price => price, :stock => stock, :logo_id => logo_id, :category_id => category_id)
+      nb_products += 1
+    end
+  end
+end
+puts "Products created #{products.size} products"
